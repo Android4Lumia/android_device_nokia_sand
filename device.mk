ace9b77576e60bc33409fce3eff832ffc3ecf608
mattschwantes <matts@coherix.com>
2019-08-12 10:55:47 +0800

    Add a missing backslash
    This was preventing Nokia logo from appearing when booting TWRP and ROM

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/fstab.qcom:recovery/root/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
+    $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.rle:root/initlogo.rle
 
 # TWRP (optional)
